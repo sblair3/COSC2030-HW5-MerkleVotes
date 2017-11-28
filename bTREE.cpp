944c3b8af3658680d6da18f71cd72e9efa753b31
Oyedola Ajao <31634220+Oyedola@users.noreply.github.com>
2017-11-27 18:48:38 -0700

    Version 2 of bTREEz

--- bTREE.cpp
+++ bTREE.cpp
@@ -1,47 +1,230 @@
-/*
-bTREE.cpp
-Sam Blair, Oyedola Ajao, and Easton Tuttle
-Computer Science II
-Homework 5 - Merkle Votes
-November 8, 2017
+//Oyedola Ajao - Group : HashBrowns
+//COSC 2030 - FALL 2017
+//MerkleVotes - Assignments 4, 5, & 6
+//Due : Dec 1 2017
 
-The definitions of the binary tree.
-*/
+#include <iostream>
 
-#include "stdafx.h"
 #include "bTREE.h"
 
+using namespace std;
 
 //look at descriptions in pMT.h for guidance on what you might need for these function to actually do
+
 bTREE::bTREE()
+
 {
+	tree = NULL;
+
+	count = 0;
 }
+
+
 
 bTREE::~bTREE()
+
 {
+	delete(tree);
 }
+
+
 
 int bTREE::dataInserted()
+
 {
 	return 0;
 }
+
+
 
 int bTREE::numberOfNodes()
+
 {
-	return 0;
+	return count;
 }
 
-int bTREE::insert(string data, int time)
+
+
+bool bTREE::insert(string data, int time)
+
 {
-	return 0;
+	return insert(tree, data, time);
 }
 
-int bTREE::find(string)
+
+
+bool bTREE::find(string input)
+
 {
-	return 0;
+	return find(tree, input);
 }
 
-string bTREE::locate(string find)
+
+
+string bTREE::locate(string input)
+
 {
-	return string();
+	if (find(input))
+	{
+		return locate(tree, input, "\n");
+	}
+	else
+	{
+		return "-";
+	}
+
+	return "\n";
+}
+
+bool bTREE::insert(treeNode *tmp, string data, int time)
+{
+	if (tmp == NULL)
+
+	{
+
+		tmp->data = data;
+
+		tmp->time = time;
+
+		count++;
+
+		return true;
+
+	}
+
+	else if (tmp->left == NULL)
+
+	{
+
+		insert(tmp->left, data, time);
+
+	}
+
+	else if (tmp->left->left == NULL)
+
+	{
+
+		insert(tmp->left->left, data, time);
+
+	}
+
+	else if (tmp->left->right == NULL)
+
+	{
+
+		insert(tmp->left->right, data, time);
+
+	}
+
+	else if (tmp->right == NULL)
+
+	{
+
+		insert(tmp->right, data, time);
+
+	}
+
+	else if (tmp->right->left == NULL)
+
+	{
+
+		insert(tmp->right->left, data, time);
+
+	}
+
+	else if (tmp->right->right == NULL)
+
+	{
+
+		insert(tmp->right->right, data, time);
+
+	}
+
+	return false;
+}
+
+bool bTREE::find(treeNode *tmp, string input)
+{
+	if (tmp->data == input)
+
+	{
+
+		return true;
+	}
+
+	if (tmp->left != NULL)
+
+	{
+
+		find(tmp->left, input);
+
+	}
+
+	if (tmp->right != NULL)
+
+	{
+
+		find(tmp->right, input);
+
+	}
+
+	return false;
+}
+
+string bTREE::locate(treeNode *tmp, string input, string dest)
+{
+	if (tmp->right != NULL)
+
+	{
+
+		locate(tmp->right, input, dest + "R");
+
+	}
+
+	if (tmp->left != NULL)
+
+	{
+
+		locate(tmp->left, input, dest + "L");
+
+	}
+
+	if (tmp->data == input)
+
+	{
+
+		return dest;
+
+	}
+
+	return dest;
+}
+
+
+bool operator==(const bTREE & lhs, const bTREE & rhs)
+{
+	if (lhs.tree->data == rhs.tree->data && lhs.tree->time == rhs.tree->time)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool operator!=(const bTREE & lhs, const bTREE & rhs)
+{
+	if (lhs.tree->data != rhs.tree->data && lhs.tree->time != rhs.tree->time)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+std::ostream & operator<<(std::ostream & out, const bTREE & p)
+{
+	cout << "Time : " << p.tree->time << "  Data : " << p.tree->data;
+
+	// TODO: insert return statement here
+	return out;
 }
--- bTREE.h
+++ bTREE.h
@@ -1,57 +1,77 @@
-/*
-bTREE.h
-Sam Blair, Oyedola Ajao, and Easton Tuttle
-Computer Science II
-Homework 5 - Merkle Votes
-November 8, 2017
-
-The interfaces for the binary tree.
-*/
+//Oyedola Ajao - Group : HashBrowns
+//COSC 2030 - FALL 2017
+//MerkleVotes - Assignments 4, 5, & 6
+//Due : Dec 1 2017
 
 #pragma once
-
-#include "stdafx.h"
-#include "list.h"
 #include <string>
+
 using namespace std;
 
+
+
 class bTREE
+
 {
+
+	struct treeNode {
+
+		string data;
+
+		int time;
+
+		treeNode *left;
+
+		treeNode *right;
+
+		bool leaf();
+
+	};
+
+
+
 private:
-	// Linked list used to hold the treeNodes together.
-	List tree;
 
-	// Any helper variables that may be needed.
-    
+	//some data structure to hold your treeNodes together ...
+
+	//DATASTUCTURE treeNodes tree;
+
+	//any helper private variables you need
+
+	int count;
+
+	treeNode *tree;
+
 public:
-    bTREE();
-    ~bTREE();
-    
-    int dataInserted();
-    int numberOfNodes();
-    
-    int insert(string, int);
-    
-    int find(string);
-    
-    string locate(string);
-    
-    
-    friend bool operator==(const bTREE& lhs, const bTREE& rhs);
-    friend bool operator!=(const bTREE& lhs, const bTREE& rhs);
 
-    friend std::ostream& operator<<(std::ostream& out, const bTREE& p);
+	bTREE();
 
-	friend bool operator ==(const bTREE& lhs, const bTREE& rhs)
-	{
-	}
+	~bTREE();
 
-	friend bool operator !=(const bTREE& lhs, const bTREE& rhs)
-	{
-	}
 
-	friend std::ostream& operator <<(std::ostream& out, const bTREE& p)
-	{
-	}
-    
+	int dataInserted();
+
+	int numberOfNodes();
+
+
+	bool insert(string, int);
+
+	bool find(string);
+
+	string locate(string);
+
+
+	bool insert(treeNode*, string, int);
+
+	bool find(treeNode*, string);
+
+	string locate(treeNode*, string, string);
+
+
+
+	friend bool operator==(const bTREE& lhs, const bTREE& rhs);
+
+	friend bool operator!=(const bTREE& lhs, const bTREE& rhs);
+
+	friend std::ostream& operator<<(std::ostream& out, const bTREE& p);
 };
